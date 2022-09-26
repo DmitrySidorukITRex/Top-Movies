b76373c2a3f5d03cd7f9ba304a04d11e91976469
contr.dmitry.sidoruk <contr.dmitry.sidoruk@pandadoc.com>
2022-09-26 22:41:32 +0400

    Add server side

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3005;
+
+mongoose.connect(
+  'mongodb+srv://Lostintime:Lostintime2415@cluster0.deszira.mongodb.net/Project',
+  { useNewUrlParser: true }
+);
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log('Server started!');
+});
